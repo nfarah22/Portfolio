e02f0fc8ac1583ad47e031a79974a67321b3bdcf
Najah Farah <najahfarah27@gmail.com>
2023-11-20 23:48:29 +0000

    nav bar

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import About from './About';
 import Portfolio from './Portfolio';
@@ -8,13 +9,17 @@ import Footer from './Footer';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- my-portfolio/src/Header.js
+++ my-portfolio/src/Header.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      </nav>
-    </header>
+    <nav>
+      <ul>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/portfolio">Portfolio</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
   );
 }
 
